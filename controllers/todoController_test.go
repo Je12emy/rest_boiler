1b0310f78dc2948eb7fc93c346a5b6de6d25e900
Jeremy <jeremy@je12emy.com>
2022-06-05 21:10:24 -0600

    test: add new test Test_Returns_200Ok_When_Creating_A_Todo

--- controllers/todoController_test.go
+++ controllers/todoController_test.go
@@ -1,8 +1,10 @@
 package controllers
 
 import (
+	"bytes"
 	"encoding/json"
 	"io/ioutil"
+	"je12emy/todo_app/dto"
 	"je12emy/todo_app/helpers"
 	"je12emy/todo_app/models"
 	"je12emy/todo_app/services"
@@ -48,10 +50,11 @@ func initRouter(t TodoController) http.Handler {
 	r := mux.NewRouter()
 	r.HandleFunc("/todo/{id}", t.GetById).Methods(http.MethodGet)
 	r.HandleFunc("/todo", t.GetAll).Methods(http.MethodGet)
+	r.HandleFunc("/todo", t.CreateTodo).Methods(http.MethodPost)
 	return r
 }
 
-func TestGetAll(t *testing.T) {
+func Test_Returns_All_Todos(t *testing.T) {
 	// Arrange
 	setup()
 	var response []models.Todo
@@ -75,7 +78,7 @@ func TestGetAll(t *testing.T) {
 	assert.Equal(t, response, todos, "Response did not match the expected result")
 }
 
-func TestGetById(t *testing.T) {
+func Test_Returns_A_Todo_By_Its_ID(t *testing.T) {
 	// Arrange
 	setup()
 	var response models.Todo
@@ -99,7 +102,7 @@ func TestGetById(t *testing.T) {
 	assert.Equal(t, response, todos[1], "Response did not match the expected result")
 }
 
-func TestGetByIdNotFound(t *testing.T) {
+func Test_Returns_NotFound_When_The_ID_Is_Not_Found(t *testing.T) {
 	// Arrange
 	setup()
 	type ApiError struct {
@@ -126,4 +129,40 @@ func TestGetByIdNotFound(t *testing.T) {
 
 	assert.Equal(t, result.StatusCode, http.StatusNotFound, "Response should have been 404 Not Found")
 	assert.Equal(t, response.Error, helpers.NotFoundError(models.TodoModelName, 99))
+}
+
+func Test_Returns_200Ok_When_Creating_A_Todo(t *testing.T) {
+	// Arrange
+	setup()
+	var response models.Todo
+	var createTodoDto = dto.CreateTodo{
+		Todo: "Buy milk",
+	}
+
+	var requestBody bytes.Buffer
+	err := json.NewEncoder(&requestBody).Encode(createTodoDto)
+
+	if err != nil {
+		t.Errorf("Expected error to be nil but got %v", err)
+	}
+
+	request := httptest.NewRequest(http.MethodPost, "/todo", &requestBody)
+	sut := router
+
+	// Act
+	sut.ServeHTTP(recorder, request)
+	result := recorder.Result()
+	defer result.Body.Close()
+
+	data, err := ioutil.ReadAll(result.Body)
+	err = json.Unmarshal(data, &response)
+
+	// Assert
+	if err != nil {
+		t.Errorf("Expected error to be nil but got %v", err)
+	}
+
+	assert.Equal(t, result.StatusCode, http.StatusOK, "Response should have been 200 Ok")
+	assert.Equal(t, response.Todo, createTodoDto.Todo, "Response Todo did not match with the request")
+	assert.Equal(t, response.Completed, false, "Response Todo did not match with the request")
 }
